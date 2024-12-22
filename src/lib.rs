88da355cd3c108a06ab6fb12dd6a70ecaa154b1b
jw1912 <jamie.whiting.dev@gmail.com>
2024-12-22 18:52:12 +0000

    alphabetical order

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@ mod compressed_move;
 mod compressed_position;
 mod training_data_file;
 
-pub mod chess;
 pub mod binpack_error;
+pub mod chess;
 pub mod reader;
 pub mod training_data_entry;
 pub mod writer;
