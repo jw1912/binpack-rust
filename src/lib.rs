bd3a3ebfbe32491fdc2e7bd91f73f560580abefd
jw1912 <jamie.whiting.dev@gmail.com>
2024-12-22 18:51:26 +0000

    Make `chess` module public Needed for doing anything with the returned `TrainingDataEntry`

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 mod arithmetic;
-mod chess;
 mod compressed_move;
 mod compressed_position;
 mod training_data_file;
 
+pub mod chess;
 pub mod binpack_error;
 pub mod reader;
 pub mod training_data_entry;
